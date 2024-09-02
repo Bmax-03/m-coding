79b6560567c1dd6034b7618aed7d741ee70875a8
acer <acer@ACER>
2024-09-02 11:04:29 +0630

    Version 1

--- src/home/com/Welcome.java
+++ src/home/com/Welcome.java
@@ -3,7 +3,7 @@ package home.com;
 public class Welcome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome from IMU University!");
 
 	}
 
